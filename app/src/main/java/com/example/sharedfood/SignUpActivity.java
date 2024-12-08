c8e86fed45fa548d7cfb7a8b6bd959c0bae2252d
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2024-12-08 19:48:31 +0200

    Your commit message

--- app/src/main/java/com/example/sharedfood/SignUpActivity.java
+++ app/src/main/java/com/example/sharedfood/SignUpActivity.java
@@ -1,12 +1,24 @@
 package com.example.sharedfood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignUpActivity extends AppCompatActivity {
 
